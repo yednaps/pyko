fe709a2022f5c981cea4585dd5d1dac4dae4b0b1
Sanjeev Pandey <yednap@gmail.com>
2015-03-06 13:14:55 -0700

    lessons 20,21,22

--- python2/koans/about_classes.py
+++ python2/koans/about_classes.py
@@ -104,7 +104,7 @@ class AboutClasses(Koan):
         self.assertEqual('Fido', fido.name)
 
     def test_args_must_match_init(self):
-        self.assertRaises("TypeError", self.Dog5)  # Evaluates self.Dog5()
+        self.assertRaises(TypeError, self.Dog5)  # Evaluates self.Dog5()
 
         # THINK ABOUT IT:
         # Why is this so?
@@ -128,7 +128,7 @@ class AboutClasses(Koan):
             #
             # Implement this!
             #
-            return __
+            return self._name
 
         def __repr__(self):
             return "<Dog named '" + self._name + "'>"
@@ -136,7 +136,7 @@ class AboutClasses(Koan):
     def test_inside_a_method_self_refers_to_the_containing_object(self):
         fido = self.Dog6("Fido")
 
-        self.assertEqual(__, fido.get_self())  # Not a string!
+        self.assertEqual(fido, fido.get_self())  # Not a string!
 
     def test_str_provides_a_string_version_of_the_object(self):
         fido = self.Dog6("Fido")
@@ -144,17 +144,17 @@ class AboutClasses(Koan):
 
     def test_str_is_used_explicitly_in_string_interpolation(self):
         fido = self.Dog6("Fido")
-        self.assertEqual(__, "My dog is " + str(fido))
+        self.assertEqual("My dog is Fido", "My dog is " + str(fido))
 
     def test_repr_provides_a_more_complete_string_version(self):
         fido = self.Dog6("Fido")
-        self.assertEqual(__, repr(fido))
+        self.assertEqual("<Dog named 'Fido'>", repr(fido))
 
     def test_all_objects_support_str_and_repr(self):
         seq = [1, 2, 3]
 
-        self.assertEqual(__, str(seq))
-        self.assertEqual(__, repr(seq))
+        self.assertEqual('[1, 2, 3]', str(seq))
+        self.assertEqual('[1, 2, 3]', repr(seq))
 
-        self.assertEqual(__, str("STRING"))
-        self.assertEqual(__, repr("STRING"))
+        self.assertEqual('STRING', str("STRING"))
+        self.assertEqual("'STRING'", repr("STRING"))
